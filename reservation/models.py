9e2acacf031091470cb1534c9cf9031f9eaf2b4e
abo3adel <abo3adel35@gmail.com>
2018-06-22 01:35:50 +0200

    Migrate tables

--- reservation/models.py
+++ reservation/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
 
 class Hotel(models.Model):
     hotel_name = models.CharField(max_length= 100)
-    hotel_city = models.Charfiled(max_length= 100)
+    hotel_city = models.CharField(max_length= 100)
     total_rooms = models.IntegerField()
     empty_rooms = models.IntegerField()
 
