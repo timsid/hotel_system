4649d5ffe6cae5348d33a7ff36381272ab6159e5
abo3adel <abo3adel35@gmail.com>
2018-06-22 01:51:48 +0200

    Init models with site admin

--- reservation/models.py
+++ reservation/models.py
@@ -11,13 +11,22 @@ class Hotel(models.Model):
     total_rooms = models.IntegerField()
     empty_rooms = models.IntegerField()
 
+    def __str__(self):
+        return self.hotel_name
+
 class Customer(models.Model):
     name = models.CharField(max_length= 100)
     phone_num = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
 
 class Reservation(models.Model):
     hotel = models.ForeignKey(Hotel)
     customer = models.ForeignKey(Customer)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.hotel.hotel_name + " for: " + self.customer.name
 
