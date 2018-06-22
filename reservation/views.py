5ada5dfde502ed0b401a8e80c7f9556db6c8af47
abo3adel <abo3adel35@gmail.com>
2018-06-22 04:23:13 +0200

    Add all hotels

--- reservation/views.py
+++ reservation/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Hotel
+
 # Create your views here.
 
 def AllHotels(request):
-    return HttpResponse("You are finally here all-hotels")
+    """ Return list of all saved hotels """
+
+    hotels_list = "<ul>"
+    for h in Hotel.objects.all():
+        hotels_list += "<li>" + h.hotel_name + "</li>"
+    hotels_list += "</li>"
+
+    return HttpResponse(hotels_list)
